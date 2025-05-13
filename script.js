2c07e022597bceac6bd6007b5cf4d2a2ee7a5446
ishika749 <ishikashah749@gmail.com>
2025-05-13 08:23:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-function indexOfIgnoreCase(str: string, subStr: string): number {
-  // If subStr is empty, return 0 as an empty string is found at the start of any string
-  if (subStr === "") {
-    return 0;
-  }
-
+function indexOfIgnoreCase(str, subStr) {
   // Convert both strings to lowercase for case-insensitive comparison
   const lowerStr = str.toLowerCase();
   const lowerSubStr = subStr.toLowerCase();
-
-  // Use indexOf to find the first occurrence of subStr in str
+  
+  // Use indexOf to find the first occurrence
   return lowerStr.indexOf(lowerSubStr);
 }
-
-
-
-
-// Please do not change the code below
-//const s1 = prompt("Enter s1:");
-//const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
